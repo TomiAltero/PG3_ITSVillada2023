83395af88490c8b480cd6ac958d10cf2ca71de79
TomiAltero <tomasaltero@gmail.com>
2023-04-14 16:58:47 -0300

    Guia 3-Ejercicio4

---
+++ Ejercicios/Guia 3 -Manejo de excepciones/Ejercicio4.py
@@ -0,0 +1,29 @@
+def ejercicio_4():
+    while True:
+        try:
+            num1 = int(input("Ingrese el numero 1:"))
+            num2 = int(input("Ingrese el numero 2:"))
+            division = num1 / num2
+            print(division)
+
+            while True:
+                entrada = input("Desea seguir haciendo calculos (si-no): ")
+
+                if entrada == "si":
+                    if entrada.isupper() or entrada.islower():
+                        continue
+                    continue
+                elif entrada == "no":
+                    return
+                else:
+                    print("Ingrese si o no")
+                    continue
+        except ValueError:
+            print("Ingrese un numero POR FAVOR")
+            print("-"*50)
+            continue
+        except ZeroDivisionError:
+            print("No se pude dividir po 0")
+            print("-"*50)
+            continue
+ejercicio_4()   
